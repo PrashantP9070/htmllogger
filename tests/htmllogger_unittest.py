efd003e0ccc6280d8a0dfcbe441e7366769af196
Sujit Nayakwadi (CW) <Sujit.Nayakwadi@blueyonder.com>
2021-07-18 08:38:45 +0530

    v0.2 removed unwanted print log added requirements, gitignore

--- tests/htmllogger_unittest.py
+++ tests/htmllogger_unittest.py
@@ -3,14 +3,15 @@ from selenium import webdriver
 from htmllogger.Htmllogger import HTMlLogger
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
+
 class InputFormsCheck2(unittest.TestCase):
 
     # Opening browser.
     def setUp(self):
-        self.logger = HTMlLogger('directory path in which report to be created')
+        self.logger = HTMlLogger('./')  # directory path in which report to be created'
         binary = FirefoxBinary('Binary Path for your browser')
         self.driver = webdriver.Firefox(firefox_binary=binary,
-                                                executable_path="driver path")
+                                        executable_path="driver path")
 
     # Testing Single Input Field.
     def test_singleInputField(self):
@@ -25,7 +26,7 @@ class InputFormsCheck2(unittest.TestCase):
             eleUserMessage = driver.find_element_by_id("user-message")
             eleUserMessage.clear()
             eleUserMessage.send_keys("Test Python")
-            self.logger.assert_step_log("Entered text [Test Python] in [user-message] EditBox.")         # Writting step log
+            self.logger.assert_step_log("Entered text [Test Python] in [user-message] EditBox.")  # Writting step log
             # Finding "Show Your Message" button element by css selector using both id and class name. And clicking it.
             eleShowMsgBtn = driver.find_element_by_css_selector('#get-input > .btn')
             eleShowMsgBtn.click()
